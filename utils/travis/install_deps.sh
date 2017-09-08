27b9d4faf22a68522450297712505172e57b9ec4
Alexander van Gessel <ai0867@gmail.com>
2017-09-08 16:05:28 +0200

    More testing

--- utils/travis/install_deps.sh
+++ utils/travis/install_deps.sh
@@ -9,22 +9,13 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
 
 else
 
-	#if [ "$CXXSTD" == "1y" ]; then
-	#	sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y;
-	#fi
+	if [ "$CXXSTD" == "1y" ]; then
+		sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y;
+	fi
 	sudo apt-get update -qq
 	sudo apt-get install -qq libboost-filesystem-dev libboost-iostreams-dev libboost-random-dev libboost-program-options-dev libboost-regex-dev libboost-system-dev libboost-test-dev libboost-locale-dev libboost-thread-dev
-echo 'foo1'
 	sudo apt-get install -qq libcairo2-dev libfribidi-dev libpango1.0-dev
-echo 'foo2'
-	sudo apt-get install -qq libsdl2-image-dev
-echo 'foo3'
-	sudo apt-get install -qq libsdl2-mixer-dev
-echo 'foo4'
-	sudo apt-get install -qq libsdl2-ttf-dev
-echo 'foo5'
-	sudo apt-get install -qq libvorbis-dev
-echo 'foo6'
+	sudo apt-get install -qq libsdl2-image-dev libsdl2-mixer-dev libsdl2-ttf-dev libvorbis-dev
 	sudo apt-get install gdb moreutils xvfb libssl-dev
 
 	if [ "$USE_CMAKE" = true  ]; then
