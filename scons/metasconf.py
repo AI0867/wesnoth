41ac6b2633b8d8052ff64dd0f4e6e6137bfb7c1c
Alexander van Gessel <ai0867@gmail.com>
2017-09-21 13:22:49 +0200

    More testing

--- scons/metasconf.py
+++ scons/metasconf.py
@@ -1,8 +1,12 @@
 # vi: syntax=python:et:ts=4
 from SCons.Script import *
+import sys
 
-def init_metasconf(env, modules):
-    modules = map(__import__, modules)
+def init_metasconf(env, module_names):
+    syspath_bak = sys.path
+    sys.path = ['scons']
+    modules = [__import__(mod_name) for mod_name in module_names]
+    sys.path = syspath_bak
     config_checks = {}
     for module in modules:
         try:
