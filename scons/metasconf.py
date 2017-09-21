3cb1b43a5a2068f0e83359dca41ce59c12f3e588
Alexander van Gessel <ai0867@gmail.com>
2017-09-21 11:49:37 +0200

    Test scons

--- scons/metasconf.py
+++ scons/metasconf.py
@@ -5,5 +5,9 @@ def init_metasconf(env, modules):
     modules = map(__import__, modules)
     config_checks = {}
     for module in modules:
-        config_checks.update(module.config_checks)
+        try:
+            config_checks.update(module.config_checks)
+        except Exception as e:
+            print("Module {} doesn't provide config_checks?".format(module.__name__))
+            raise
     return config_checks
